812b1ed65d497f3631d88fdf4677e6a06fd83a27
YuanZhewei <yuenzhewei@gmail.com>
2015-09-02 20:31:34 +0800

    add vundle config in install config script

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ SOURCE_PATH=`pwd`
 ln -sf $SOURCE_PATH/vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 
-ln -sf $SOURCE_PATH/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
