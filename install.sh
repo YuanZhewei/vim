74fcd2a00f935ec5ed9dbb9f0cfb86d771410f76
Yuan Zhewei <ace@localhost.localdomain>
2015-09-04 13:28:39 +0800

    more automatic script

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -sf $SOURCE_PATH/vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall
+
+cp $SOURCE_PATH/self.vim ~/.vim/bundle/Airline/autoload/airline/themes
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --gocode-completer
