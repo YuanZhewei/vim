445b73bf2aa0aafe83d1160f7568e5eb98bd7828
Yuan Zhewei <yuenzhewei@gmail.com>
2018-01-15 20:50:00 +0800

    add macos

--- install.sh
+++ install.sh
@@ -1,48 +1,50 @@
 #/bin/bash
 
+OS="$(uname -s)"
 string="$(lsb_release -a 2>/dev/null |grep "Distributor ID")"
 RELEASE=$(echo $string | awk '{print $NF}')
 echo $RELEASE
 SOURCE_PATH=`pwd`
 
 install_package() {
-    if [ x"$RELEASE" = x"Ubuntu" ];
+    if [ x"OS" = x"Darwin" ];
+    then
+        check_and_install_mac $1
+    elif [ x"$RELEASE" = x"Ubuntu" ];
     then
         check_and_install_ubuntu $1
     fi
 }
 
+check_and_install_mac() {
+    brew install $1
+}
+
 check_and_install_ubuntu() {
-    flag=$(dpkg -l | grep $1)
-    if [ x"" = x"$flag" ];
-    then
-        apt-get install -y $1
-    fi
+    apt-get install -y $1
 }
 
 install_package git
 install_package cmake
 install_package python-dev
 
-if [ -z "$(ls ~/.vimrc)" ];
+if [ ! -f "~/.vimrc" ];
 then
-    echo 1
     ln -sf $SOURCE_PATH/vimrc ~/.vimrc
 fi
 
-if [ -z "$(ls ~/.vim/bundle)" ];
+if [ ! -d "~/.vim/bundle" ];
 then
-    echo 1
     mkdir -p ~/.vim/bundle
 fi
 
-if [ -z "$(ls ~/.vim/bundle/Vundle.vim)" ];
+if [ ! -f "~/.vim/bundle/Vundle.vim" ];
 then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-#vim +PluginInstall +qall
+vim +PluginInstall +qall
 
-if [ -z "$(ls ~/.vim/bundle/Airline/autoload/airline/themes)" ];
+if [ ! -f " ~/.vim/bundle/Airline/autoload/airline/themes" ];
 then
     cp $SOURCE_PATH/self.vim ~/.vim/bundle/Airline/autoload/airline/themes
 fi
